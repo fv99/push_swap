ab0831604b68b4dce69835e0db0c0cb74f4d95ee
practical <filipvon@protonmail.com>
2023-03-28 17:25:10 +0200

    Added list test functions

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 15:47:57 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/03/24 15:55:40 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/03/28 17:24:01 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,55 @@ int	you_fucked_up(char *msg)
 	exit(1);
 }
 
+t_list	*initialize_list(int argc, char **argv)
+{
+	t_list	*lst;
+	t_list	*tmp;
+	int		i;
+	int		num;
+
+	i = 1;
+	lst = NULL;
+	while (i < argc)
+	{
+		num = ft_atoi(argv[i]);
+		if (num > INT_MAX || num < INT_MIN)
+			you_fucked_up("Integer read failure");
+		tmp = ft_lstnew((void *)(intptr_t)num);
+		if (tmp == NULL)
+			you_fucked_up("Failed to allocate list");
+		ft_lstadd_back(&lst, tmp);
+		i++;
+	}
+	return (lst);
+}
+
+
+void	test_list_read(t_list *lst)
+{
+	t_list	*buf;
+
+	buf = lst;
+	while (buf != NULL)
+	{
+		ft_printf("%s ", (char *) buf->content);
+		buf = buf->next;
+	}
+	ft_printf("\n");
+}
 
 int	main(int argc, char **argv)
 {
+	t_list	*stack;
+
 	ft_printf("Hello world! %s", argv[1]);
 	if (argc < 2)
 		you_fucked_up("Invalid number of arguments");
+	else
+	{
+		stack = initialize_list(argc, argv);
+		test_list_read(stack);
+
+	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fvonsovs <fvonsovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 15:48:10 by fvonsovs          #+#    #+#             */
-/*   Updated: 2023/03/24 15:52:35 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2023/03/28 16:23:16 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@
 # include <stdio.h>
 
 // structs go here
-
+typedef	struct s_stack
+{
+	t_list	*a;
+	t_list	*b;
+	int		size_a;
+	int		size_b;
+}	t_stack;
 
 
 
