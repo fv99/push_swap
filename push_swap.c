16c370ab4acf1b486ed865d19c5254df0b5b57ed
fvonsovs <filipvon@protonmail.com>
2023-03-29 16:36:09 +0200

    Fixed segfault, stack reading now works

--- push_swap.c
+++ push_swap.c
@@ -37,6 +37,8 @@ t_stack	*stack_last(t_stack *stack)
 {
 	t_stack	*tmp;
 
+	if (!stack)
+		return (NULL);
 	tmp = stack;
 	while (tmp->next)
 	{
@@ -52,17 +54,32 @@ void	stackadd_back(t_stack **stack, t_stack *new)
 {
 	t_stack	*tmp;
 
-	if (!*stack)
-		*stack = new;
-	else
+	if (*stack)
 	{
 		tmp = stack_last(*stack);
 		tmp->next = new;
 		new->next = NULL;
 	}
+	else
+	{
+		*stack = new;
+		(*stack)->next = NULL;
+	}
 }
 
+// will free array of strings created by ft_split
+void	free_split(char **str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
 
 void	initialize_list(t_stack **lst, int argc, char **argv)
 {
@@ -70,6 +87,7 @@ void	initialize_list(t_stack **lst, int argc, char **argv)
 	char	**args;
 	int		i;
 
+	*lst = NULL;
 	i = 0;
 	if (argc == 2)
 		args = ft_split(argv[1], ' ');
@@ -77,10 +95,15 @@ void	initialize_list(t_stack **lst, int argc, char **argv)
 	{
 		i = 1;
 		args = argv;
+	}
+	while (args[i])
+	{
 		tmp = new_stack(ft_atoi(args[i]));
 		stackadd_back(lst, tmp);
 		i++;
 	}
+	if (argc == 2)
+		free_split(args);
 }
 
 
@@ -92,31 +115,49 @@ void	test_stack_read(t_stack **a)
     {
         return;
     }
-
     buf = *a;
     while (buf != NULL)
     {
-        ft_printf("%d ", buf->num);
+        ft_printf("%i ", buf->num);
         buf = buf->next;
     }
     ft_printf("\n");
 }
 
+void	free_stack(t_stack **stack)
+{
+	t_stack	*tmp;
+	t_stack	*next;
+
+	if (!stack || !*stack)
+		return;
+	tmp = *stack;
+	while (tmp)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*stack = NULL;
+	free(stack);
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	**a;
-	t_stack	**b;
+	// t_stack	**b;
 
 	if (argc < 2)
 		you_fucked_up("Invalid number of arguments");
 	else
 	{
 		a = (t_stack **)malloc(sizeof(t_stack));
-		b = (t_stack **)malloc(sizeof(t_stack));
+		// b = (t_stack **)malloc(sizeof(t_stack));
 		initialize_list(a ,argc, argv);
 		test_stack_read(a);
-		test_stack_read(b);
-
+		// test_stack_read(b);
+		free_stack(a);
+		// free_stack(b);
 	}
 	return (0);
 }
